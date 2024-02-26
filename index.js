f23272e5edef2a6241a7cbbd3741c613d7eb7924
TomScott <contact@tascott.co.uk>
2024-02-26 22:11:28 +0000

    Starter files and basic API call

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('Hello World!')
+
+//log all the text of all the paragraphs on the page
+const paragraphs = document.querySelectorAll('p')
+paragraphs.forEach(p => {
+    //add class to each paragraph
+    p.classList.add('paragraph-translate')
+    if(p.textContent === '' || p.textContent.split(' ').length <= 1) {
+        return
+    }
+    //TODO: split the paragraph in to sentences and the sentences in to short phrases
+    console.log(p.textContent.split(' ').length + ': ' + p.textContent)
+    p.addEventListener('click',() => {
+        translate(p);
+    })
+})
+
+function translate(paragraph) {
+    var sourceLang = 'en';
+    var targetLang = 'es';
+    var sourceText = paragraph.textContent;
+    console.log(paragraph.textContent)
+
+    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+    console.log(url);
+
+    $.getJSON(url,function(data) {
+        console.log(data[0][0][0]);
+        paragraph.textContent = paragraph.textContent + ' (' + data[0][0][0] + ')';
+    });
+}
