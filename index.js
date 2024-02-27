d925b7843db9da6f05d31078e51010aca95dd43d
TomScott <contact@tascott.co.uk>
2024-02-27 20:56:03 +0000

    Replace splitting paragraphs with finding specific nodes. Move out styling/listener function

--- index.js
+++ index.js
@@ -1,34 +1,71 @@
-console.log('Hello World!')
+function wrapTextNodesWithSpans(element) {
+    // Iterate through all child nodes of the element, which will be a paragraph
+    Array.from(element.childNodes).forEach((node) => {
+        if(node.nodeType === Node.TEXT_NODE) {
+            // This is a text node, apply regex and wrap parts in <span>
+            const parts = node.nodeValue.match(/[^,;:.]+[,;:.]?|\s+/g);
+            if(parts) {
+                const spanWrapper = document.createDocumentFragment();
+                parts.forEach(part => {
+                    if(part.trim() !== '') {
+                        const span = document.createElement('span');
+                        span.textContent = part;
+                        spanWrapper.appendChild(span);
+                    } else {
+                        // Preserve whitespace
+                        spanWrapper.appendChild(document.createTextNode(part));
+                    }
+                });
+                node.parentNode.replaceChild(spanWrapper,node);
+            }
+        } else if(node.nodeType === Node.ELEMENT_NODE && node.nodeName !== 'A') {
+            // This is an element node that is not an <a> tag, recurse
+            wrapTextNodesWithSpans(node);
+        }
+        // If it's an <a> tag or other non-text element, do nothing and leave it intact
+    });
 
-//log all the text of all the paragraphs on the page
-const paragraphs = document.querySelectorAll('p')
-paragraphs.forEach(p => {
-    //add class to each paragraph
-    p.classList.add('paragraph-translate')
-    if(p.textContent === '' || p.textContent.split(' ').length <= 1) {
-        return
-    }
-    //TODO: split the paragraph in to sentences and the sentences in to short phrases
-    console.log(p.textContent.split(' ').length + ': ' + p.textContent)
-    p.addEventListener('click',() => {
-        translate(p);
-    })
-})
+    styleSpansAndAddListenters(element);
+}
 
-function translate(paragraph) {
+// Apply the function to your paragraph element
+console.log('wrapTextNodesWithSpans')
+document.querySelectorAll('p').forEach(p => {
+    wrapTextNodesWithSpans(p);
+});
+
+
+
+function translate(span) {
     var sourceLang = 'en';
     var targetLang = 'es';
-    var sourceText = paragraph.textContent;
-    console.log(paragraph.textContent)
+    var sourceText = span.textContent;
+    console.log('SPAN CONTENT:', span.textContent)
 
     var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
     console.log(url);
 
     $.getJSON(url,function(data) {
         console.log(data[0][0][0]);
-        // TODO: insert the translation into the paragraph after the original text, in brackets
+        // TODO: insert the translation into the span after the original text, in brackets
         // TODO: add a class to the translated text
         // TODO: show some stylised link between the original and the translated text
-        paragraph.textContent = paragraph.textContent + ' (' + data[0][0][0] + ')';
+        span.textContent = span.textContent + ' (' + data[0][0][0] + ')';
+        //TODO add a class to the translated text span to stop it being able to be clicked and translated again
     });
+}
+
+function styleSpansAndAddListenters(p) {
+    const spans = p.querySelectorAll('span')
+    spans.forEach((span,i) => {
+        if(i % 2 === 0) {
+            span.classList.add('even')
+        } else {
+            span.classList.add('odd')
+        }
+        span.addEventListener('click',() => {
+            translate(span);
+        })
+    })
+
 }
